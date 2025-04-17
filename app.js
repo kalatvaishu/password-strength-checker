76e92cf07bd5a7de6ec079fbda550b21610cbd4c
kalatvaishu <kalatvaishali09@gmail.com>
2025-04-17 23:24:25 +0530

    Password Strength Checker
    Created Password Strength Checker using HTML,CSS and JavaScript.

---
+++ app.js
@@ -0,0 +1,86 @@
+const passwordInput=document.querySelector("#password");
+const togglepassword=document.querySelector("#togglepassword");
+const submitbtn=document.querySelector("#submitbtn");
+const criteriaList=document.querySelector("#criteriaList");
+const togglepasswordconfirm = document.querySelector("#togglepasswordconfirm");
+const confirmpassowrd = document.querySelector("#confirmpassowrd");
+
+
+const letterCriteria = document.querySelector("#letter");
+const capitalCritera = document.querySelector("#capital");
+const numberCritera = document.querySelector("#number");
+const symbolCritera = document.querySelector("#symbol");
+const lengthCritera = document.querySelector("#length");
+const letterRegex = /[a-z]/;
+const capitalRegex = /[A-Z]/;
+const numberRegex = /[0-9 ]/;
+const symbolRegex = /[@$!%*?&#]/;
+
+
+togglepassword.addEventListener("click", (e) => {
+    const type = passwordInput.type === "password" ? "text" : "password";
+    passwordInput.type = type;
+    togglepassword.textContent = type === "password" ? "👁️" : "🙈";
+
+  });
+  togglepasswordconfirm.addEventListener("click", () => {
+    const type = confirmpassowrd.type === "password" ? "text" : "password";
+    confirmpassowrd.type = type;
+    togglepasswordconfirm.textContent = type === "password" ? "👁️" : "🙈";
+  });
+
+  passwordInput.addEventListener("input",(e)=>{
+    const value =passwordInput.value
+
+    //cheak for lowercase letters
+
+    if(letterRegex.test(value)){
+      letterCriteria. classList.remove("invalid");
+      letterCriteria.classList.add("valid");
+    }else{
+      lengthCritera.classList.remove("valid");
+      lengthCritera.classList.add("invalid")
+    }
+    //cheak for capital letters
+    if(capitalRegex.test(value)){
+      capitalCritera.classList.remove("invalid");
+      capitalCritera.classList.add("valid");
+    }else{
+      capitalCritera.classList.remove("valid");
+      capitalCritera.classList.add("invalid");
+    }
+    //cheak for number
+    if (numberRegex.test(value)) {
+      numberCritera.classList.remove("invalid");
+      numberCritera.classList.add("valid");
+    } else {
+      numberCritera.classList.remove("valid");
+      numberCritera.classList.remove("invalid");
+    }
+    
+//cheak for symbol
+  //check for sybol
+  if (symbolRegex.test(value)) {
+    symbolCritera.classList.remove("invalid");
+    symbolCritera.classList.add("valid");
+  } else {
+    symbolCritera.classList.remove("valid");
+    symbolCritera.classList.add("invalid");
+  }
+  
+  //cheak for >=8
+
+if (value.length >= 8) {
+  lengthCritera.classList.remove("invalid");
+  lengthCritera.classList.add("valid");
+} else {
+  lengthCritera.classList.remove("valid");
+  lengthCritera.classList.add("invalid");
+}
+
+
+  });
+  submitbtn.addEventListener("click",()=>{
+    criteriaList.style.display = "none"; 
+    alert("Data.submitted successfully");
+  })
